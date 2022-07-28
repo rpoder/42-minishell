9f8d15bdf3d8b74a0ea614486d9e733c00d7c122
Ronan <rpoder@student.42paris.fr>
2022-07-28 16:18:00 +0200

    ok

--- src/metachar_interpreter.c
+++ src/metachar_interpreter.c
@@ -6,7 +6,7 @@
 /*   By: ronanpoder <ronanpoder@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/22 14:02:08 by ronanpoder        #+#    #+#             */
-/*   Updated: 2022/07/28 15:37:35 by ronanpoder       ###   ########.fr       */
+/*   Updated: 2022/07/28 16:17:36 by ronanpoder       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static void	fill_with_dollar_value(t_data *data, char *src, int i, int j)
 	}
 }
 
-void	fill_interpreted_arg(t_data *data, char *src, char *dst, int len)
+void	fill_interpreted_arg(t_data *data, char *src, int len)
 {
 	int	i;
 	int	j;
@@ -43,31 +43,27 @@ void	fill_interpreted_arg(t_data *data, char *src, char *dst, int len)
 	j = 0;
 	while (src[i])
 	{
-		printf("str[%d] = %c et ", i, src[i]);
-		printf("%d\n", j);
 		quotes = set_quotes(src[i],quotes);
 		if (src[i] == '$' && is_to_interpret(src, i, quotes.sgl_quote, quotes.dbl_quote))
 		{
 			fill_with_dollar_value(data, src, i, j);
 			tmp = dollar_value_len(data, src, i + 1);
-			printf("dollar_value_len = %d\n", tmp);
 			if (tmp > 0)
 				i = i + dollar_key_len(src, i + 1);
 			j = j + tmp;
 		}
 		else
 		{
-			dst[j] = src[i];
+			data->prompt_line[j] = src[i];
 			j++;
 		}
 		i++;
 	}
-	dst[j] = '\0';
+	data->prompt_line[j] = '\0';
 }
 
 char *metachar_interpreter(t_data *data, char *src)
 {
-	char	*dst;
 	int		dst_len;
 
 	printf("src = %s\n", src);
@@ -77,11 +73,11 @@ char *metachar_interpreter(t_data *data, char *src)
 	data->prompt_line = malloc(sizeof(char) * (dst_len + 1));
 	if (!data->prompt_line)
 		return (NULL);
-	fill_interpreted_arg(data, src, dst, dst_len);
+	fill_interpreted_arg(data, src, dst_len);
 
 
-	printf("data->prompt_line = |%s|\n", dst);
+	printf("---------data->prompt_line = |%s|\n", data->prompt_line);
 	printf("len_final data->prompt_line = |%d|\n", ft_strlen(data->prompt_line));
-	return (dst);
+	return (data->prompt_line);
 }
 
