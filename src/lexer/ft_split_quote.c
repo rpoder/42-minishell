bc4cdf6d783f267869914cab8235906e3b0ddea5
Margot POURREY <mpourrey@e1r9p10.clusters.42paris.fr>
2022-08-16 19:13:40 +0200

    start fill dst

--- src/lexer/ft_split_quote.c
+++ src/lexer/ft_split_quote.c
@@ -6,13 +6,39 @@
 /*   By: mpourrey <mpourrey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 21:45:16 by mpourrey          #+#    #+#             */
-/*   Updated: 2022/08/16 16:34:48 by mpourrey         ###   ########.fr       */
+/*   Updated: 2022/08/16 19:12:45 by mpourrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	get_and_skip_token(char **token, char *str, int i, int token_start)
+{
+	
+	
+	
+}
 
+char	**fill_dst(char **dst, char *str)
+{
+	int	i;
+	int	start_token;
+	int	k;
+
+	i = 0;
+	token_start = 0;
+	k = 0;
+	while (str[i])
+	{
+		if (is_split_separator(str, i))
+		{
+			dst[k] = get_token(str, i, token_start);
+			i = skip_separator(str, i);
+			k++;
+			token_start = i;	
+		}
+	}
+}
 
 static int	count_words(char *str, int count)
 {
@@ -48,6 +74,9 @@ char	**ft_split_quote(char *str)
 	int		count;
 
 	count = count_words(str, 0);
+	dst = malloc (sizeof(char *) * (count + 1));
+	//proteger
+	dst = fill_dst(dst, str);
 
 	return (dst);
 }
