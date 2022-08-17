805a0cd6ed9312d04aa31291587697cee2cdc2b0
Margot POURREY <mpourrey@e1r10p5.clusters.42paris.fr>
2022-08-17 11:48:13 +0200

    test

--- src/lexer/ft_split_quote.c
+++ src/lexer/ft_split_quote.c
@@ -6,39 +6,11 @@
 /*   By: mpourrey <mpourrey@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 21:45:16 by mpourrey          #+#    #+#             */
-/*   Updated: 2022/08/16 19:12:45 by mpourrey         ###   ########.fr       */
+/*   Updated: 2022/08/17 11:48:10 by mpourrey         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int	get_and_skip_token(char **token, char *str, int i, int token_start)
-{
-	
-	
-	
-}
-
-char	**fill_dst(char **dst, char *str)
-{
-	int	i;
-	int	start_token;
-	int	k;
-
-	i = 0;
-	token_start = 0;
-	k = 0;
-	while (str[i])
-	{
-		if (is_split_separator(str, i))
-		{
-			dst[k] = get_token(str, i, token_start);
-			i = skip_separator(str, i);
-			k++;
-			token_start = i;	
-		}
-	}
-}
 
 static int	count_words(char *str, int count)
 {
